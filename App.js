97e0658b4752e662b746bdef46a56dc4135e955d
Marrick Chan <marrickchan@gmail.com>
2019-09-29 16:09:52 -0400

    Add rows to scrollview and add touch tooltip

--- App.js
+++ App.js
@@ -2,26 +2,29 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView } from 'react-native';
 import PokemonRegion from './src/components/PokemonRegion';
 
-export default class HelloWorldApp extends Component {
+export default class Pokedex extends Component {
   render() {
     return (
       <React.Fragment>
-        <View style={{ flex: 0.25 }}>
-          <View style={{ flex: 4, justifyContent: "center", alignItems: "center" }}>
-            <Text style={this.styles.appTitle}>I guess this will be a Pokemon App!</Text>
+        <View style={{ flex: 0.15 }}>
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={this.styles.appTitle}>Pokedex</Text>
           </View>
         </View>
 
         <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={{ flex: 2 }} >
+          contentInsetAdjustmentBehavior='automatic'
+          style={ this.styles.scrollView } >
           
-          <View style={{ flex: 2, justifyContent: "center", alignItems: "center" }}>
-            <PokemonRegion title="Kanto Pokemon" region="Kanto"/>
-            <Text>{"\n\n"}</Text>
+          {/* Pokemon from Kanto Region */}
+          <Text style={ this.styles.sectionTitle }> {'\nKanto Pokemon\n'} </Text>
+          <View style={ this.styles.pokemonView }>
+            <PokemonRegion region='Kanto'/>
           </View>
-          <View style={{ flex: 2, justifyContent: "center", alignItems: "center" }}>
-            <PokemonRegion title="Johto Pokemon" region="Johto"/>
+          {/* Pokemon from Johto Region */}
+          <Text style={ this.styles.sectionTitle }> {'\nJohto Pokemon\n'} </Text>
+          <View style={ this.styles.pokemonView }>
+            <PokemonRegion region='Johto'/>
           </View>
 
         </ScrollView>
@@ -30,8 +33,31 @@ export default class HelloWorldApp extends Component {
   }
 
   styles = {
+    /* Text */
     appTitle: {
-      fontSize: 24
+      fontSize: 36,
+      fontWeight: 'bold',
+    },
+    sectionTitle: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      justifyContent: 'center',
+      color: 'white',
+      backgroundColor: 'blue'
+    },
+
+    /* Views */
+    scrollView: {
+      marginBottom: 50,
+      flex: 1
+    },
+    pokemonView: {
+      flex: 2,
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'row',
+      flexWrap: 'wrap'
     }
   }
 }
--- src/components/PokemonRegion.js
+++ src/components/PokemonRegion.js
@@ -1,42 +1,85 @@
 import React from 'react';
-import { Image, Text } from 'react-native';
+import { Image, Modal, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native';
 
 class PokemonRegion extends React.Component {
     constructor(props){
         super(props);
     }
 
-    kantoArr = ['001', '002', '003', '004', '005', '006', '007', '008', '009'];
-    johtoArr = ['155', '156', '157', '158', '159', '160', '161', '162', '163']
+    state = {
+        modalVisible: false,
+        modalData: {
+            pokemonName: '',
+            pokedexNumber: '',
+            atkStat: '',
+            defStat: '',
+            hpStat: ''
+        }
+    }
 
-    renderPokemonImages(region) {
+    pokedexArr = [];
+
+    toggleModal(visible) {
+        this.setState({ modalVisible: visible });
+     }
+
+    fillArray(region) {
         switch (region) {
             case 'Kanto':
-                return (
-                    this.kantoArr.map(pokedexNum => (
-                        <Image
-                            style={{width: 150, height: 150, resizeMode: 'contain' }}
-                            source={{uri: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/' + pokedexNum + '.png' }}
-                        />
-                    ))
-                )
+                this.pokedexArr = [];
+                for(var i = 1; i <= 151; i++){
+                    var itempStr = '' + i;
+                    if(itempStr.length == 1){
+                        this.pokedexArr.push('00' + itempStr);
+                    } else if (itempStr.length == 2){
+                        this.pokedexArr.push('0' + itempStr);
+                    } else if (itempStr.length == 3){
+                        this.pokedexArr.push(itempStr);
+                    }
+                }
+                return;
             case 'Johto':
-                return (
-                    this.johtoArr.map(pokedexNum => (
-                        <Image
-                            style={{width: 150, height: 150, resizeMode: 'contain' }}
-                            source={{uri: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/' + pokedexNum + '.png' }}
-                        />
-                    ))
-                )
+                this.pokedexArr = [];
+                for(var i = 152; i <= 251; i++){
+                    this.pokedexArr.push(i + '');
+                }
+                return;
         }
+    }
+
+    renderPokemonImages() {
+        return (
+            this.pokedexArr.map(pokedexNum => ( 
+                <TouchableOpacity
+                    key={ 'poke' + pokedexNum }
+                    onPress={ () => this.toggleModal(!this.state.modalVisible) } >
+                    <Image
+                        style={{width: 100, height: 100, resizeMode: 'contain' }}
+                        resizeMethod={ 'resize' }
+                        source={{uri: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/' + pokedexNum + '.png' }}
+                    />
+                </TouchableOpacity>
+            ))
+        )
     }
 
     render() {
         return (
             <React.Fragment>
-                <Text>{this.props.title + "\n\n" }</Text>
-                {this.renderPokemonImages(this.props.region)}
+                <Modal 
+                    animationType = {"slide"}
+                    transparent = {false}
+                    visible = {this.state.modalVisible}>
+                    <View>
+                        <Text>Modal is open!</Text>
+                        <TouchableHighlight onPress = {() => {
+                            this.toggleModal(!this.state.modalVisible)}}>
+                            <Text>Close Modal</Text>
+                        </TouchableHighlight>
+                    </View>
+                </Modal>
+                {this.fillArray(this.props.region)}
+                {this.renderPokemonImages()}
             </React.Fragment>
         )
     }
